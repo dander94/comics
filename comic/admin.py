a070aca89d35726b51a6ddb68099e5b4dfed0e48
Ale <3balejandro@gmail.com>
2015-10-02 13:17:40 +0200

    Se crea una plantilla propia para la pagina. Se añade la funcionalidad de Localizaciones, incluyendo su creacion y su listado.

--- comic/admin.py
+++ comic/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Location,Collection,Comic
+
+admin.site.register(Location)
+admin.site.register(Collection)
+admin.site.register(Comic)
--- comic/urls.py
+++ comic/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
-urlpatterns = [url(r'^$', views.IndexView.as_view(), name="index"),
+urlpatterns = [url(r'^$', views.index, name="index"),
+		url(r'^location/$', views.LocationView.as_view(),name='location'),
+url(r'^location/create/$', views.crear,name='create_loc')
 ]
